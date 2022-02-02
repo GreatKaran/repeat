4ad8786c94dab550b67639f76e20fb0caa775c0c
GreatKaran <86833061+GreatKaran@users.noreply.github.com>
2022-02-02 14:43:30 +0530

    Update Scamtag.py

--- Scamtag.py
+++ Scamtag.py
@@ -3,7 +3,7 @@ from telethon.sync import TelegramClient
 from telethon import functions, types
 api_ids = [11462244] #add more when need u gey
 api_hashs = ['cc99e2d4b6d2e803500bee18a416ab21']
-string_session = ['1AZWarzwBu2zIkae1R3x7nuWgrRVCD64CHkX0VOua3uLchewuXFi9gL3-KKdcGwLwntljMpnS-1EOTXrRhj36V_z2w_fmSGS7LHLxEuFuRKslaAcBI9lEvHJhxXReR1qQvZE50FEF4glNqSRfH8dHb1HyQuP1Oj6hN_ZL9BGFBRKdeRxCa5D2-hnnFsuQ7EbpmVoTLFEcj9-wCR8np1WQVtxkCeAIlWzR8UU6qvlNX3pBbZcGEMNihZcAQnnAzwHrFWgoE8EsyZJe1EHhQ4r0OGMDgK8cpvqh9dK85ptE3BqDnKMt2XjMzn8HN75Tg0haS5rAqBkGlnjx8LzqX1vE2nKb1TQcTp4=']
+string_session = ['1AZWarzMBu4SKr0K_MKw7Zz1tAVya1O7alrt7dpvmyVMjjUavvJR1MmOLp2EIPYppKYYzT3P4-g5kHMh8cwTj6HzqaSAubqHS4jZyeCMSZ9V9qS-vhmz4ol-5Z4wc2c0Ok5mUovh3b_9HwHv-CokIDWLaBT66pzWlidw54OXJ0NbqRvJhhEYS5ACssMbUQGc6-D5_6SafbjWuO64zkosBsNVpdqz5SsdNFOk11O0xpaBaf2Wv4t8Tub9W4o2FiuKsbnKLnIxok3032IlLSvKOuLGTs98h3mYND-VAtjQOJugLg39UHFwrRH2AYA68uvVKaNeox7C0uzwgHA1to57PKkhZzyng-Bg=']
 for i in range(0,len(api_ids)):
     api_id = api_ids[i]
     api_hash = api_hashs[i]
